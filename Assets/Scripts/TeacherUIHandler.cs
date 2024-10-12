6a8cb136ada125d778e23aca39079390dd89dc41
Shivansh Mourya <96974868+SommeanDev@users.noreply.github.com>
2024-10-12 20:17:00 +0530

    updated room list

--- Assets/Scripts/TeacherUIHandler.cs
+++ Assets/Scripts/TeacherUIHandler.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using Firebase.Database;
 using TMPro;
 using UnityEngine;
@@ -8,6 +10,8 @@ public class TeacherUIHandler : MonoBehaviour
 {
     [SerializeField] private TMP_Text teacherName;
     [SerializeField] private TMP_Dropdown statusDropdown;
+    [SerializeField] private TMP_Text floorText;
+    [SerializeField] private TMP_Dropdown floorDropdown;
     [SerializeField] private TMP_Text currentRoomText;
     [SerializeField] private TMP_Dropdown currentRoomDropdown;
     
@@ -24,6 +28,33 @@ public class TeacherUIHandler : MonoBehaviour
         _currentTeacherStatus = _reference.Child("teacher").Child(PlayerPrefs.GetString("UserId")).Child("Status").GetValueAsync().ToString();
         currentRoomDropdown.gameObject.SetActive(false);
         currentRoomText.gameObject.SetActive(false);
+        floorText.gameObject.SetActive(false);
+        floorDropdown.gameObject.SetActive(false);
+    }
+
+    public void Update()
+    {
+        int currentFloor = Convert.ToInt32(floorDropdown.options[floorDropdown.value].text);
+        List<TMP_Dropdown.OptionData> currentRooms = new List<TMP_Dropdown.OptionData>();
+
+        for(int i = 1; i < 13; i++)
+        {
+            //StringBuilder stringBuilder = new StringBuilder();
+            int roomNo = currentFloor * 100 + i;
+            string roomNoStr = roomNo.ToString();
+            TMP_Dropdown.OptionData newOptionData = new TMP_Dropdown.OptionData();
+            newOptionData.text = roomNoStr;
+            currentRooms.Add(newOptionData);
+        }
+        
+        currentRoomDropdown.options = currentRooms;
+    }
+
+    private void FixedUpdate()
+    {
+        _updates["Status"] = _currentTeacherStatus;
+        _updates["CurrentRoomNo"] = currentRoomDropdown.options[currentRoomDropdown.value].text;
+        //Debug.Log(_updates["CurrentRoomNo"]);
     }
 
     public void EnableRoomSelection(int statusId)
@@ -33,11 +64,13 @@ public class TeacherUIHandler : MonoBehaviour
         {
             currentRoomText.gameObject.SetActive(true);
             currentRoomDropdown.gameObject.SetActive(true);
-            _updates["Status"] = _currentTeacherStatus;
-            _updates["CurrentRoomNo"] = currentRoomDropdown.options[currentRoomDropdown.value].text;
+            floorDropdown.gameObject.SetActive(true);
+            floorText.gameObject.SetActive(true);
         }
         else
         {
+            floorDropdown.gameObject.SetActive(false);
+            floorText.gameObject.SetActive(false);
             currentRoomText.gameObject.SetActive(false);
             currentRoomDropdown.gameObject.SetActive(false);
         }
@@ -55,7 +88,7 @@ public class TeacherUIHandler : MonoBehaviour
             {
                 Debug.Log("Teacher data updated successfully");
                 _messageHandler.SendNotification("Teacher Status Update",
-                    $"Teacher status updated to: {_currentTeacherStatus}");
+                    $"Teacher status updated to: {_currentTeacherStatus} Room no: {currentRoomDropdown.options[currentRoomDropdown.value].text}");
             }
         });
         
